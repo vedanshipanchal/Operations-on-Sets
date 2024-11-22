9234431aca9b791b2e3db569a840022b21fdc183
vedanshipanchal <vedanshipanchal@gmail.com>
2024-11-22 16:26:00 +0000

    Operations on Sets

---
+++ main.py
@@ -0,0 +1,15 @@
+x = {1,2,3,4,5,5,5,5}
+print(x)
+
+y = {"vedanshi",True,3,4.5}
+print(y)
+
+z = set([1,2,3,2])
+print(z)
+
+n = set([0,1,2,3,4])
+print("before removing:",n)
+n.pop()
+print("after removing",n)
+n.remove(2)
+print("removing number 7:",n)
